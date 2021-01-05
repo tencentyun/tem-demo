b5d30ee86a3283b9729671660944f12a5c03f18e
Tony Han <tonyhan@tencent.com>
2021-01-05 16:01:45 +0800

    add app name and host name to pong

--- consul-provider/src/main/java/com/example/consul/provider/Application.java
+++ consul-provider/src/main/java/com/example/consul/provider/Application.java
@@ -1,5 +1,7 @@
 package com.example.consul.provider;
 
+import com.example.common.AppName;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -8,9 +10,12 @@ import org.springframework.web.bind.annotation.RestController;
 @SpringBootApplication
 @RestController
 public class Application {
+	@Value("${spring.application.name}")
+	private String appName;
+
 	@RequestMapping("/ping")
-	public String home() {
-		return "pong";
+	public String ping() {
+		return "pong from " + AppName.name(appName) + "\n";
 	}
 
 	public static void main(String[] args) {
--- eureka-provider/src/main/java/com/example/eureka/provider/Application.java
+++ eureka-provider/src/main/java/com/example/eureka/provider/Application.java
@@ -1,19 +1,26 @@
 package com.example.eureka.provider;
 
+import com.example.common.AppName;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.net.InetAddress;
+import java.net.UnknownHostException;
+
 @SpringBootApplication
 @EnableEurekaClient
 @RestController
 public class Application {
+    @Value("${spring.application.name}")
+    private String appName;
 
     @RequestMapping("/ping")
-    public String home() {
-        return "pong";
+    public String ping() {
+        return "pong from " + AppName.name(appName) + "\n";
     }
 
     public static void main(String[] args) {
